3f61a6f9b9524c8c0ce6bdc686bc86ff510a13fe
Павел Санников <pavelsan95@mail.ru>
2023-04-13 22:14:42 +0500

    Добавьте файлы проекта.

---
+++ HomeApi.Data/Repos/RoomRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeApi.Data.Repos
+{
+    /// <summary>
+    /// Репозиторий для операций с объектами типа "Room" в базе
+    /// </summary>
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly HomeApiContext _context;
+        
+        public RoomRepository (HomeApiContext context)
+        {
+            _context = context;
+        }
+        
+        /// <summary>
+        ///  Найти комнату по имени
+        /// </summary>
+        public async Task<Room> GetRoomByName(string name)
+        {
+            return await _context.Rooms.Where(r => r.Name == name).FirstOrDefaultAsync();
+        }
+        
+        /// <summary>
+        ///  Добавить новую комнату
+        /// </summary>
+        public async Task AddRoom(Room room)
+        {
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                await _context.Rooms.AddAsync(room);
+            
+            await _context.SaveChangesAsync();
+        }
+        /// <summary>
+        ///  Изменить существующую комнату
+        /// </summary>
+        public async Task UpdateRoomByName(Room new_room, string name)
+        {
+            Room room = await GetRoomByName(name);
+            room = new_room;
+            var entry = _context.Entry(room);
+
+            if (entry.State == EntityState.Detached)
+                _context.Rooms.Update(room);
+
+            // Сохраняем изменения в базе 
+            await _context.SaveChangesAsync();
+        }
+    }
+}
